52742729fb435aa6966c238e2c4e782058fdfbe1
emsyoungg <emily03young@gmail.com>
2025-03-31 12:47:56 +0100

    var changes

--- VARmodel.py
+++ VARmodel.py
@@ -15,21 +15,23 @@ class VARForecaster:
         self.forecaster = VAR()
 
     def fit(self):
-        print("Unique value counts per feature:\n", self.train_data[self.features].nunique())
-
-        self.forecaster.fit(self.train_data[self.features])
+        self.forecaster.fit(self.train_data)
 
     def predict(self, steps=6):
         fh = ForecastingHorizon(range(1, steps + 1), is_relative=True)
         return self.forecaster.predict(fh)
 
     def plot_forecast(self, forecasts):
+        patient_data_combined = pd.concat([self.train_data, self.test_data], axis=0)
 
         plt.figure(figsize=(12, 8))
         for i, feature in enumerate(self.features):
+            actual_series = patient_data_combined[feature]
+            forecast_series = forecasts[feature]
+
             plt.subplot(len(self.features), 1, i + 1)
-            plt.plot(self.test_data.index, self.test_data[feature], label=f"Actual {feature}", color="blue")
-            plt.plot(forecasts.index, forecasts[feature], label=f"Predicted {feature}", color="red", linestyle="--")
+            plt.plot(actual_series.index, actual_series, label=f"Actual {feature}", color="blue")
+            plt.plot(forecast_series.index, forecast_series,label=f"Predicted {feature}", color="red", linestyle="--")
             plt.title(f"{feature} Forecast (VAR Model)")
             plt.legend()
         plt.tight_layout()
