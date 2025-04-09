9c828077886bd30a2e5e0fa8d001e7909601fadd
emsyoungg <emily03young@gmail.com>
2025-04-09 15:41:31 +0100

    multivariate lstm forecasts

--- LSTM/LSTMmodel2.py
+++ LSTM/LSTMmodel2.py
@@ -1,9 +1,11 @@
+import pandas as pd
+from matplotlib import pyplot as plt
 from sktime.forecasting.base import ForecastingHorizon
 from sktime.forecasting.neuralforecast import NeuralForecastLSTM
 
 
 class LSTMforecaster2:
-    def __init__(self, target, exogenous, y_train, y_test=None, X_train=None, X_test=None):
+    def __init__(self, target, y_train,  exogenous=None, y_test=None, X_train=None, X_test=None):
         self.target = target
         self.exogenous = exogenous
         self.y_train = y_train
@@ -26,3 +28,30 @@ class LSTMforecaster2:
     def fit_predict(self):
         #fh = ForecastingHorizon([1, 2, 3, 4, 5, 6])
         return self.forecaster.fit_predict(y=self.y_train, X=self.X_train, X_pred=self.X_test)
+
+    def plot_forecast(self, forecasts, patient_id, steps=6):
+        """
+        Plots the forecasted data alongside the actual data for a specific patient.
+
+        Args:
+            patient_id (int): Patient ID for visualisation.
+            steps (int): Number of forecast steps.
+            :param forecasts: forcast data
+        """
+
+        # patient to visualise
+        patient_predictions = forecasts.loc[patient_id]
+        patient_data_combined = pd.concat([self.y_train, self.y_test], axis=0)
+        patient_data = patient_data_combined.loc[patient_id]
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(patient_data.index, patient_data, label=f"Actual", marker="o")
+        plt.plot(patient_predictions.index, patient_predictions, label=f"Forecasted",
+                 marker="x", linestyle="--",
+                 color="orange")
+        plt.xlabel("ICULOS (Time Index)")
+        plt.ylabel(f"{self.target} Value")
+        plt.title(f"Forecast for Last {steps} Hours of Patient {patient_id}")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
